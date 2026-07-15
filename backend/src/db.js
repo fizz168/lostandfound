3c776f4ae4277f3c6beea7db0c1f6a24a5895969
Melkinator <aacrazykid@yahoo.com>
2026-07-15 15:26:09 +0700

    fix

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,14 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-// Use DATABASE_URL if available, otherwise use individual variables
 let sequelize;
 if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
     logging: false,
     dialectOptions: {
-      ssl: { require: true, rejectUnauthorized: false }
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
   });
 } else {
